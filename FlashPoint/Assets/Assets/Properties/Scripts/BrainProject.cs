952ac8ab18db4d826549b406dfd6ac0ecb7c7b14
Andrieiev1 <132195597+Andrieiev1@users.noreply.github.com>
2025-01-23 22:02:58 +0200

    Changes

---
+++ FlashPoint/Assets/Assets/Properties/Scripts/BrainProject.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using Unity.UI;
+using UnityEngine.UI;
+public class BrainProject : MonoBehaviour
+{
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+        
+    }
+}
